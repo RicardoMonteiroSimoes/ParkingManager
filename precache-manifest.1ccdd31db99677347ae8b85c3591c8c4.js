03051940b6df5208e894ff62f4d27d8638da8a8c
RicardoMonteiroSimoes <ricardo_simoes_93@hotmail.com>
2020-08-17 13:17:40 +0000

    Deploying to gh_page from  @ ef5ac98f5b7bf146dc1e8b3f8b424ea6a6884446 🚀

---
+++ precache-manifest.1ccdd31db99677347ae8b85c3591c8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fb5b9dc11d0f574a20d02792909ce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa37666244cc9e7495d",
+    "url": "/static/css/main.835a8d9d.chunk.css"
+  },
+  {
+    "revision": "e0773a457a5627adc085",
+    "url": "/static/js/2.d70e1d2f.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "/static/js/2.d70e1d2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aaa37666244cc9e7495d",
+    "url": "/static/js/main.9248a30e.chunk.js"
+  },
+  {
+    "revision": "5a80399875c5b0b384ca",
+    "url": "/static/js/runtime-main.eefcae9d.js"
+  }
+]);
